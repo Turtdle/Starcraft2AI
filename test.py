3df9e7646f3e725cb376e0a04c8014b88e7246bc
Sam <sam@wanfamily.org>
2023-12-13 19:15:40 -0800

    added redundancy

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import Bot
-
+import os
 for i in range(0,100):
     try:
         Bot.run_bot_from_file("saves3/MakeMoney" + str(i) + ".h5", i)
@@ -7,3 +7,11 @@ for i in range(0,100):
         Bot.run_bot_from_file("saves3/MakeMoney" + str(i) + ".h5", i)
         print(e)
         continue
+
+for i in range(3):
+    for i in range(0,100):
+        #checks if there is a file saves3/MakeMoneyi.txt
+        #if there isnt, it will run the bot and create one
+        if not os.path.exists("saves3/MakeMoney" + str(i) + ".h5"):
+            Bot.run_bot_from_file("saves3/MakeMoney" + str(i) + ".h5", i)
+
